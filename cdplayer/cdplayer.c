533fbfc522f65963c59a20e802eb3e24de07abc4
Tom Tromey <tromey@src.gnome.org>
1998-03-09 02:47:58 +0000

    Include GNOME_INCLUDEDIR.
    	* Makefile.am (INCLUDES): Include GNOME_INCLUDEDIR.
    
    	* Makefile.am (libpanel_applet_la_SOURCES): Added mico-parse.cc.
    	(panel_SOURCES): Likewise.
    	* cdplayer/cdplayer.c (main): Use new gnome_init.
    	* logout.c (main): Use new gnome_init.
    	* clock.c (main): Use new gnome_init.
    	* applet-lib.cc (gnome_panel_applet_init_corba): Use
    	panel_initialize_corba.
    	* mico-parse.cc: New file.
    	* mico-glue.cc (panel_corba_gtk_main): Use
    	panel_initialize_corba.
    	* main.c (init_session_management): Removed.
    	(main): Use new session and argument parsing code.
    	* mico-glue.h (corba_gtk_main): Removed.
    	(panel_corba_register_arguments): Declare.

--- cdplayer/cdplayer.c
+++ cdplayer/cdplayer.c
@@ -306,7 +306,8 @@ main(int argc, char **argv)
 	GtkWidget *cdplayer;
 	char *result;
 	
-	gnome_init("cdplayer_applet", &argc, &argv);
+	panel_corba_register_arguments ();
+	gnome_init("cdplayer_applet", NULL, argc, argv, 0, NULL);
 
 	if (!gnome_panel_applet_init_corba (&argc, &argv)){
 		fprintf (stderr, "Could not comunicate with the panel\n");
