426b6a612e2dd9173fc02c56a7d23eab06bf7b33
Arturo Espinosa <unammx@src.gnome.org>
1998-05-26 01:54:45 +0000

    Add political doit-faire

--- icewm-pager/wmpager_applet.c
+++ icewm-pager/wmpager_applet.c
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
     {
         /* FIXME: This should check to make sure icewm (or equiv) is running.
            Not sure if it is working though... */
-        g_error(_("This applet requires you to run an icewm development snapshot.\n"));
+        g_error(_("This applet requires you to run a window manager with the XA_ extensions.\n"));
         exit(1);
     }
 
