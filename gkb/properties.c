f456800fcbadf4792b092610c7657e6a0b83812b
Szabolcs Ban <bansz@src.gnome.org>
1998-11-26 13:33:05 +0000

    Added xmodmap support. Removed bad path-calculation.

--- gkb/properties.c
+++ gkb/properties.c
@@ -1,4 +1,4 @@
-/* properties dialog from GKB applet
+/* properties dialog for GKB applet
  */
 
 #include <stdio.h>
@@ -7,7 +7,6 @@
 #include <gnome.h>
 
 #include "properties.h"
-#include "gkbpath.h"
 
 GtkWidget *propbox=NULL;                                                        
 
@@ -24,22 +23,34 @@ static gchar *wpFileName=NULL;
 static gint cur;                                                                
                                                                                 
 extern GtkWidget *disp;                                                         
-extern gkb_properties pr;                                                       
+extern gkb_properties pr;
 
 static GtkWidget * pix1, * pix2;
 
 void
 load_properties( char *path, gkb_properties *prop )
 {
+        char *apath, *apixmap;
 
 	gnome_config_push_prefix (path);
 	prop->dmap[0]	= gnome_config_get_string ("gkb/dmap0=us");
 	prop->dmap[1]	= gnome_config_get_string ("gkb/dmap1=hu");
-	prop->dfile[0]	= gnome_config_get_string ("gkb/dfile0=" D_PATH "/pixmaps/gkb/us.xpm");
-	prop->dfile[1]	= gnome_config_get_string ("gkb/dfile1=" D_PATH "/pixmaps/gkb/hu.xpm");
+	prop->dcmd	= gnome_config_get_string ("gkb/dcmd=setxkbmap");
+/*
+        Sorry, I made a bad change last time...
+*/
+        apixmap         = gnome_pixmap_file("gkb/us.xpm");    
+	apath           = g_copy_strings("gkb/dfile0=", apixmap, NULL);
+        prop->dfile[0]  = gnome_config_get_string (apath);
+        g_free(apixmap); g_free(apath);
+
+	apixmap         = gnome_pixmap_file("gkb/hu.xpm");
+	apath           = g_copy_strings("gkb/dfile1=", apixmap, NULL);
+	prop->dfile[1]  = gnome_config_get_string (apath);
+	g_free(apixmap); g_free(apath);
+	
 	gnome_config_pop_prefix ();
 }
-
 
 void
 save_properties( char *path, gkb_properties *prop )
@@ -47,6 +58,7 @@ save_properties( char *path, gkb_properties *prop )
 	gnome_config_push_prefix (path);
 	gnome_config_set_string( "gkb/dmap0",  prop->dmap[0] );
 	gnome_config_set_string( "gkb/dmap1",  prop->dmap[1] );
+	gnome_config_set_string( "gkb/dcmd",   prop->dcmd );
 	gnome_config_set_string( "gkb/dfile0", prop->dfile[0] );
 	gnome_config_set_string( "gkb/dfile1", prop->dfile[1] );
         gnome_config_pop_prefix ();
@@ -150,11 +162,24 @@ apply_callback(GtkWidget * pb, gint page, gpointer data)
   temp_props.dmap[0] = g_strdup(ent1);
   temp_props.dmap[1] = g_strdup(ent2);
 
-  
+
   memcpy( &pr, &temp_props, sizeof(gkb_properties) );
   gnome_pixmap_load_file (GNOME_PIXMAP (pixmap), pr.dfile[curpix]);
 
 }
+
+static void ch_xkb_cb(GtkWidget *widget, gpointer data)
+{
+      temp_props.dcmd = g_strdup("setxkbmap");
+      gnome_property_box_changed(GNOME_PROPERTY_BOX(propbox)); 
+}
+
+static void ch_xmodmap_cb(GtkWidget *widget, gpointer data)
+{
+        temp_props.dcmd = g_strdup("xmodmap");
+        gnome_property_box_changed(GNOME_PROPERTY_BOX(propbox));
+}
+
 
 gint
 destroy_cb( GtkWidget *widget, void *data )
@@ -168,11 +193,17 @@ void
 properties(AppletWidget *applet, gpointer data)
 {
 	GtkWidget * frame;
+        GtkWidget * nbox;
         GtkWidget * box;
         GtkWidget * but1, * but2;
         GtkWidget * ent1, * ent2;
         GtkWidget * vbx1, * vbx2;
+        GtkWidget *omenu;
+        GtkWidget *menu;
+        GtkWidget *item;
 
+
+        nbox= gtk_vbox_new (TRUE,0);
     	box= gtk_hbox_new  (TRUE,0);
 	vbx1= gtk_vbox_new (FALSE,0);
     	vbx2= gtk_vbox_new (FALSE,0);
@@ -188,7 +219,7 @@ properties(AppletWidget *applet, gpointer data)
 	gtk_window_set_title(GTK_WINDOW(propbox), _("GKB settings"));
 	gtk_window_set_policy(GTK_WINDOW(propbox), FALSE, FALSE, TRUE);
 
-	gtk_container_border_width( GTK_CONTAINER(box), 1 );
+	gtk_container_border_width( GTK_CONTAINER(nbox), 1 );
 
         ent1 = gtk_entry_new_with_max_length (5);
         gtk_object_set_data(GTK_OBJECT(propbox),"e1",ent1);
@@ -240,8 +271,28 @@ properties(AppletWidget *applet, gpointer data)
 	gtk_box_pack_start( GTK_BOX(box), vbx1,FALSE,FALSE,1 );
 	gtk_box_pack_start( GTK_BOX(box), vbx2,FALSE,FALSE,1 );
 
+        omenu = gtk_option_menu_new ();
+        menu = gtk_menu_new ();
+
+        item = gtk_menu_item_new_with_label(_("Xkb"));
+        gtk_signal_connect (GTK_OBJECT (item), "activate", (GtkSignalFunc) ch_xkb_cb, NULL);
+        gtk_menu_append (GTK_MENU (menu), item);
+
+        item = gtk_menu_item_new_with_label(_("Xmodmap"));
+        gtk_signal_connect (GTK_OBJECT (item), "activate", (GtkSignalFunc) ch_xmodmap_cb, NULL);
+        gtk_menu_append (GTK_MENU (menu), item);
+
+        gtk_option_menu_set_history (GTK_OPTION_MENU (omenu), (strcmp("xmodmap",pr.dcmd)?0:1));
+        printf("--%d--",(strcmp("xmodmap",pr.dcmd)?0:1));fflush(stdout);
+        gtk_option_menu_set_menu (GTK_OPTION_MENU (omenu), menu);
+
+        gtk_box_pack_start( GTK_BOX(nbox), box, TRUE, TRUE, 5);
+	gtk_box_pack_start( GTK_BOX(nbox), omenu,FALSE,FALSE,1 );
+
+        gtk_widget_show (omenu);
+
 	gtk_notebook_append_page (GTK_NOTEBOOK(GNOME_PROPERTY_BOX (propbox)->notebook),
-				  box, gtk_label_new (_("Menu")));
+				  nbox, gtk_label_new (_("Menu")));
 
         gtk_signal_connect( GTK_OBJECT(propbox),
 		"apply", GTK_SIGNAL_FUNC(apply_callback), NULL );
