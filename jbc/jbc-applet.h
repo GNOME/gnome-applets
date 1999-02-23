c34f8d8a788082fc358ddbbcb61f316a8a65abb9
jona <jona@src.gnome.org>
1999-02-23 06:04:23 +0000

    Trying this commit again..
    Trying this commit again..

---
+++ jbc/jbc-applet.h
@@ -0,0 +1,18 @@
+/* $Id$
+ *
+ * Includes for jbc-applet.c
+ *
+ */
+
+#include "pixmaps/t0.xpm"
+#include "pixmaps/t1.xpm"
+#include "pixmaps/t2.xpm"
+#include "pixmaps/t3.xpm"
+#include "pixmaps/t4.xpm"
+#include "pixmaps/t5.xpm"
+#include "pixmaps/t6.xpm"
+#include "pixmaps/t7.xpm"
+#include "pixmaps/t8.xpm"
+#include "pixmaps/t9.xpm"
+#include "pixmaps/tcolon.xpm"
+#include "pixmaps/tbcolon.xpm"
