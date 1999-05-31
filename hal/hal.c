94b75380db92e972d0b255af1075801a351b4457
Rachel Hestilow <hestgray@src.gnome.org>
1999-05-31 22:14:21 +0000

    *** empty log message ***

--- hal/hal.c
+++ hal/hal.c
@@ -224,7 +224,7 @@ static void about_cb(AppletWidget * widget, gpointer data)
 	authors[2] = NULL;
 
 	fish->aboutbox =
-	    gnome_about_new(_("The GNOME HAL"), "2001",
+	    gnome_about_new(_("The GNOME HAL"), "9000",
 			    _("(C) 1999 the Free Software Foundation"),
 			    (const char **) authors,
 			    _("I am a HAL 9000 computer, Production "
