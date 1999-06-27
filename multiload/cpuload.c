905baef48707f760d6c113166c9bf1e5ac947b48
Jacob Berkman <jberkman@andrew.cmu.edu>
1999-06-27 18:04:12 +0000

    don't do a stock_callback, just a normal one (fixes a crash)
    1999-06-27  Jacob Berkman  <jberkman@andrew.cmu.edu>
    
    	* cdplayer/cdplayer.c (main): don't do a stock_callback, just
    	a normal one (fixes a crash)
    
    	* multiload/swapload.c (make_swapload_applet): ditto
    
    	* multiload/cpuload.c (make_cpuload_applet): same
    
    	* multiload/memload.c (make_memload_applet): one more time

--- multiload/cpuload.c
+++ multiload/cpuload.c
@@ -65,11 +65,10 @@ make_cpuload_applet (const gchar *goad_id)
 					   multiload_properties_cb,
 					   g);
 
-    applet_widget_register_stock_callback (APPLET_WIDGET(applet),
-					   "run_gtop",
-					   NULL,
-					   _("Run gtop..."),
-					   start_gtop_cb, NULL);
+    applet_widget_register_callback (APPLET_WIDGET(applet),
+				     "run_gtop",
+				     _("Run gtop..."),
+				     start_gtop_cb, NULL);
 
     return applet;
 }
