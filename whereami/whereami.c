95974dcd454cfc260369481e0a09a9a6138e134f
John Kodis <kodis@src.gnome.org>
1999-08-22 13:59:41 +0000

    Reapplied change to use upper-case state enum names.

--- whereami/whereami.c
+++ whereami/whereami.c
@@ -25,7 +25,7 @@
 #include <applet-widget.h>
 
 static int x, y;
-static enum { idle, position, size } state;
+static enum { IDLE, POSITION, SIZE } state;
 
 static char *idle_msg = N_("Where\nAm I?");
  
@@ -50,13 +50,13 @@ motion_handler(GtkWidget *widget, GdkEventMotion *motion, gpointer data)
 
   switch (state)
     {
-    case idle:
+    case IDLE:
       sprintf(where, "%+d\n%+d", x, y);
       break;
-    case position:
+    case POSITION:
       sprintf(where, "%+d\n%+d", x = motion->x_root, y = motion->y_root);
       break;
-    case size:
+    case SIZE:
       sprintf(where, "%+d\n%+d", (int)motion->x_root-x, (int)motion->y_root-y);
       break;
     }
@@ -67,7 +67,7 @@ motion_handler(GtkWidget *widget, GdkEventMotion *motion, gpointer data)
 static void
 button_handler(GtkWidget *widget, GdkEventButton *button, gpointer data)
 {
-  if (state == idle && button->type == GDK_BUTTON_RELEASE)
+  if (state == IDLE && button->type == GDK_BUTTON_RELEASE)
     {
       GdkEventMask events =  GDK_POINTER_MOTION_MASK |
 	GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK;
@@ -79,17 +79,17 @@ button_handler(GtkWidget *widget, GdkEventButton *button, gpointer data)
       gdk_cursor_destroy(cursor);
       gdk_window_get_pointer(NULL, &x, &y, NULL);
       motion_handler(widget, NULL, NULL);
-      state = position;
+      state = POSITION;
     }
 
-  if (state == position && button->type == GDK_BUTTON_PRESS)
-    state = size;
+  if (state == POSITION && button->type == GDK_BUTTON_PRESS)
+    state = SIZE;
 
-  if (state == size && button->type == GDK_BUTTON_RELEASE)
+  if (state == SIZE && button->type == GDK_BUTTON_RELEASE)
     {
       gdk_pointer_ungrab(GDK_CURRENT_TIME);
       gtk_label_set_text(GTK_LABEL(GTK_BIN(widget)->child), idle_msg);
-      state = idle;
+      state = IDLE;
     }
 }
 
