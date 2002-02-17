ff08bb6020bf446729070c8203c8915146eb737a
Kjartan Maraas <kmaraas@src.gnome.org>
2002-02-17 08:45:28 +0000

    Fix #include <config.h>

--- sound-monitor/pvd/esdpvd.c
+++ sound-monitor/pvd/esdpvd.c
@@ -6,6 +6,10 @@
  *
  */
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include "esdpvd.h"
 
 static GList *stream_list = NULL;
--- sound-monitor/pvd/esdpvd.h
+++ sound-monitor/pvd/esdpvd.h
@@ -6,7 +6,6 @@
  *
  */
 
-#include <config.h>
 #include <gnome.h>
 
 #include <esd.h>
