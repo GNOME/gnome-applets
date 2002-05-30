55dc7977f1521dd6f47e6832882fc10929728619
Kevin Vandersloot <kfv101@psu.edu>
2002-05-30 04:13:40 +0000

    fix up temp regex so that it can get the temp even if the dew is not
    2002-05-30  Kevin Vandersloot <kfv101@psu.edu>
    
            * weather.c: fix up temp regex so that it can get the temp
            even if the dew is not available. Fixes bug #82335

--- gweather/weather.c
+++ gweather/weather.c
@@ -287,7 +287,7 @@ close_cb (GnomeVFSAsyncHandle *handle, GnomeVFSResult result, gpointer data)
 #define WIND_RE_STR  "^(([0-9]{3})|VRB)([0-9]?[0-9]{2})(G[0-9]?[0-9]{2})?KT$"
 #define VIS_RE_STR   "^(([0-9]?[0-9])|(M?1/[0-9]?[0-9]))SM$"
 #define CLOUD_RE_STR "^(CLR|BKN|SCT|FEW|OVC)([0-9]{3})?$"
-#define TEMP_RE_STR  "^(M?[0-9][0-9])/(M?[0-9][0-9])$"
+#define TEMP_RE_STR  "^(M?[0-9][0-9])/(M?(//|[0-9][0-9]))$"
 #define PRES_RE_STR  "^(A|Q)([0-9]{4})$"
 #define COND_RE_STR  "^(-|\\+)?(VC|MI|BC|PR|TS|BL|SH|DR|FZ)?(DZ|RA|SN|SG|IC|PE|GR|GS|UP|BR|FG|FU|VA|SA|HZ|PY|DU|SQ|SS|DS|PO|\\+?FC)$"
 
