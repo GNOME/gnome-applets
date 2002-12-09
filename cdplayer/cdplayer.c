28727438a92bdc29b53524732990e1d11ffa03ec
Kevin Vandersloot <kfv101@psu.edu>
2002-12-09 02:30:11 +0000

    fix segfault on exit when we don't need to stop the cd.
    2002-12-08  Kevin Vandersloot <kfv101@psu.edu>
    
    	* cdplayer.c: fix segfault on exit when we don't need to stop the cd.

--- cdplayer/cdplayer.c
+++ cdplayer/cdplayer.c
@@ -243,7 +243,8 @@ cdplayer_destroy(GtkWidget * widget, gpointer data)
         gtk_timeout_remove(cd->timeout);
     cd->timeout = 0;
     /* Since the applet is being destroyed, stop playing cd */
-    cdrom_stop(cd->cdrom_device);
+    if(cd_try_open(cd))
+        cdrom_stop(cd->cdrom_device);
     cd_close (cd);
 
     tooltips = g_object_get_data (G_OBJECT (cd->panel.applet), "tooltips");
