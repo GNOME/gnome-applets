c3cebeff256e49cd2c488cbadc5ebf3cb646ab1b
George Lebl <jirka@5z.com>
2003-09-15 22:18:35 +0000

    when started in lockdown mode, hide the preferences item
    Mon Sep 15 15:16:38 2003  George Lebl <jirka@5z.com>
    
    	* cdplayer.c: when started in lockdown mode, hide the preferences
    	  item

--- cdplayer/cdplayer.c
+++ cdplayer/cdplayer.c
@@ -212,6 +212,17 @@ applet_fill (PanelApplet *applet)
                                        applet_menu_verbs,
                                        cd);
 
+    if (panel_applet_get_locked_down (PANEL_APPLET (applet))) {
+	    BonoboUIComponent *popup_component;
+
+	    popup_component = panel_applet_get_popup_component (PANEL_APPLET (applet));
+
+	    bonobo_ui_component_set_prop (popup_component,
+					  "/commands/Preferences",
+					  "hidden", "1",
+					  NULL);
+    }
+
     component = panel_applet_get_popup_component (PANEL_APPLET (applet));
 
     g_signal_connect (component, "ui-event", G_CALLBACK (ui_component_event), cd);
