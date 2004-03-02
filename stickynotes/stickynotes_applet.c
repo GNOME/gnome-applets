be66cabe5688bf3173f276a8df44a7bbca823679
Dennis Smit <ds@nerds-incorporated.org>
2004-03-02 12:11:11 +0000

    Submit patch:
    2004-03-02  Dennis Smit  <ds@nerds-incorporated.org>
    
    Submit patch:
    
    2004-03-02  Kaushal Kumar  <kaushal.kumar@wipro.com>
    
    * sticyknotes_applet.c (stickynotes_applet_update_prefs):
    Fix a compilation error. (Bugzilla #135820)

--- stickynotes/stickynotes_applet.c
+++ stickynotes/stickynotes_applet.c
@@ -338,20 +338,26 @@ void stickynotes_applet_update_icon(StickyNotesApplet *applet)
 
 void stickynotes_applet_update_prefs()
 {
+	gint height, click_behavior;
+	gboolean sys_color, sys_font, sticky, force_default;
+	gchar *font_str;
+	GdkColor color;
 	gint width = gconf_client_get_int(stickynotes->gconf, GCONF_PATH "/defaults/width", NULL);
+
 	width = MAX (width, 1);
-	gint height = gconf_client_get_int(stickynotes->gconf, GCONF_PATH "/defaults/height", NULL);
+	height = gconf_client_get_int(stickynotes->gconf, GCONF_PATH "/defaults/height", NULL);
 	height = MAX (height, 1);
-	gboolean sys_color = gconf_client_get_bool(stickynotes->gconf, GCONF_PATH "/settings/use_system_color", NULL);
-	gboolean sys_font = gconf_client_get_bool(stickynotes->gconf, GCONF_PATH "/settings/use_system_font", NULL);
-	gint click_behavior = gconf_client_get_int(stickynotes->gconf, GCONF_PATH "/settings/click_behavior", NULL);
-	gboolean sticky = gconf_client_get_bool(stickynotes->gconf, GCONF_PATH "/settings/sticky", NULL);
-	gboolean force_default = gconf_client_get_bool(stickynotes->gconf, GCONF_PATH "/settings/force_default", NULL);
-	gchar *font_str = gconf_client_get_string(stickynotes->gconf, GCONF_PATH "/defaults/font", NULL);
+
+	sys_color = gconf_client_get_bool(stickynotes->gconf, GCONF_PATH "/settings/use_system_color", NULL);
+	sys_font = gconf_client_get_bool(stickynotes->gconf, GCONF_PATH "/settings/use_system_font", NULL);
+	click_behavior = gconf_client_get_int(stickynotes->gconf, GCONF_PATH "/settings/click_behavior", NULL);
+	sticky = gconf_client_get_bool(stickynotes->gconf, GCONF_PATH "/settings/sticky", NULL);
+	force_default = gconf_client_get_bool(stickynotes->gconf, GCONF_PATH "/settings/force_default", NULL);
+	font_str = gconf_client_get_string(stickynotes->gconf, GCONF_PATH "/defaults/font", NULL);
+
 	if (!font_str)
 		font_str = g_strdup ("Sans 10");
 	
-	GdkColor color;
 	{
 		gchar *color_str = gconf_client_get_string(stickynotes->gconf, GCONF_PATH "/defaults/color", NULL);
 		if (!color_str)
