38eb7d6777e95d410f7653d7e8645e73d4b7c289
Kevin Vandersloot <kfv101 psu edu>
2004-04-25 18:19:33 +0000

    Fix for certain invalid conditions. From bug #134452. Patch from Arnaud
    2004-04-25  Kevin Vandersloot <kfv101 psu edu>
    
    	* weather.c: Fix for certain invalid conditions. From bug #134452. Patch
    	from Arnaud Patard .

--- gweather/weather.c
+++ gweather/weather.c
@@ -298,7 +298,7 @@ close_cb (GnomeVFSAsyncHandle *handle, GnomeVFSResult result, gpointer data)
 #define TIME_RE_STR  "^([0-9]{6})Z$"
 #define WIND_RE_STR  "^(([0-9]{3})|VRB)([0-9]?[0-9]{2})(G[0-9]?[0-9]{2})?KT$"
 #define VIS_RE_STR   "^(([0-9]?[0-9])|(M?1/[0-9]?[0-9]))SM$"
-#define CLOUD_RE_STR "^(CLR|BKN|SCT|FEW|OVC)([0-9]{3})?$"
+#define CLOUD_RE_STR "^(CLR|BKN|SCT|FEW|OVC|SKC)([0-9]{3})?$"
 #define TEMP_RE_STR  "^(M?[0-9][0-9])/(M?(//|[0-9][0-9]))$"
 #define PRES_RE_STR  "^(A|Q)([0-9]{4})$"
 #define COND_RE_STR  "^(-|\\+)?(VC|MI|BC|PR|TS|BL|SH|DR|FZ)?(DZ|RA|SN|SG|IC|PE|GR|GS|UP|BR|FG|FU|VA|SA|HZ|PY|DU|SQ|SS|DS|PO|\\+?FC)$"
@@ -526,7 +526,13 @@ static gboolean metar_tok_cloud (gchar *tokp, WeatherInfo *info)
     gint alt = -1;
 
     if (regexec(&metar_re[CLOUD_RE], tokp, 0, NULL, 0) == REG_NOMATCH)
-        return FALSE;
+	if (!strcmp(tokp,"CAVOK"))
+	{
+		info->sky=SKY_CLEAR;
+		return TRUE;
+	}
+   	else
+        	return FALSE;
 
     strncpy(stype, tokp, 3);
     stype[3] = 0;
@@ -537,6 +543,8 @@ static gboolean metar_tok_cloud (gchar *tokp, WeatherInfo *info)
     }
 
     if (!strcmp(stype, "CLR")) {
+        info->sky = SKY_CLEAR;
+    } else if (!strcmp(stype, "SKC")) {
         info->sky = SKY_CLEAR;
     } else if (!strcmp(stype, "BKN")) {
         info->sky = SKY_BROKEN;
