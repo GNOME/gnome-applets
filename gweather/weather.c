4f9d6ebeb5a0a74c728061f502fa148ac4f6b825
Dennis Smit <ds@nerds-incorporated.org>
2004-06-07 21:59:28 +0000

    Initializing some variables to fix crasher on location change.
    2004-06-07  Dennis Smit  <ds@nerds-incorporated.org>
    
    * weather.c (weather_info_config_read):
    Initializing some variables to fix crasher on
    location change.
    
    Patch by: Gareth Owen  <gowen72@yahoo.com>
    
    Fixes bug #14386

--- gweather/weather.c
+++ gweather/weather.c
@@ -1915,7 +1915,15 @@ WeatherInfo *weather_info_config_read (PanelApplet *applet)
     info->radar = NULL;  /* FIX */
     info->requests_pending = FALSE;
     info->metar_buffer = NULL;
-    info->iwin_buffer = NULL; 
+    info->iwin_buffer = NULL;
+
+    info->met_buffer = NULL;
+    info->bom_buffer = NULL;
+
+    info->met_handle = NULL;
+    info->iwin_handle = NULL;
+    info->met_handle = NULL;
+    info->bom_handle = NULL;
 
     return info;
 }
