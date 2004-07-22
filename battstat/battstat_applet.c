f6937fc3141fd82c97082a3a278a47f9d3584d72
Davyd Madeley <davyd@ucc.asn.au>
2004-07-22 13:26:35 +0000

    Fix references to certain apm_info structs that don't exist in FreeBSD.
    2004-07-22  Davyd Madeley  <davyd@ucc.asn.au>
    
            * battstat/battstat_applet.c: Fix references to certain apm_info
            structs that don't exist in FreeBSD. Patch from Joe Marcus Clarke
            <marcus@freebsd.org>. Closes #148048.

--- battstat/battstat_applet.c
+++ battstat/battstat_applet.c
@@ -474,7 +474,11 @@ pixmap_timeout( gpointer data )
    ) {
       /* Warn that battery dropped below red_val */
       if(battery->lowbattnotification) {
+#ifdef __FreeBSD__
+	 new_string = get_remaining (apminfo.ai_batt_time);
+#else
 	 new_string = get_remaining (apminfo.battery_time);
+#endif
 	 new_label = g_strdup_printf (_("Your battery is running low (%d%%, %s). You should recharge your battery to avoid losing your work."), batt_life, new_string);
 	 g_free (new_string);
 	 battery->lowbattnotificationdialog = gtk_dialog_new_with_buttons (
@@ -733,7 +737,11 @@ pixmap_timeout( gpointer data )
       }
       
       if (batt_state != 3)
+#ifdef __FreeBSD__
+	      rem_time = get_remaining(apminfo.ai_batt_time);
+#else
 	      rem_time = get_remaining(apminfo.battery_time);
+#endif
       else
 	      rem_time = g_strdup(_(status[batt_state]));
       
