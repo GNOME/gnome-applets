fb0978d4fe94ebe3362ee9f40b46c933cbd5a073
Kjartan Maraas <kmaraas@gnome.org>
2004-10-20 12:20:46 +0000

    Use GETTEXT_PACKAGE so that translations work.
    2004-10-20  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* gswitchit-plugins.c: (main): Use GETTEXT_PACKAGE
    	so that translations work.

--- gswitchit/gswitchit-plugins.c
+++ gswitchit/gswitchit-plugins.c
@@ -320,9 +320,9 @@ main (int argc, char **argv)
 	GError *gconf_error = NULL;
 	GConfClient *confClient;
 
-	bindtextdomain (PACKAGE, GNOMELOCALEDIR);
+	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-	textdomain (PACKAGE);
+	textdomain (GETTEXT_PACKAGE);
 	memset (&gswic, 0, sizeof (gswic));
 	gnome_program_init ("gswitchit-properties", VERSION,
 			    LIBGNOMEUI_MODULE, argc, argv,
