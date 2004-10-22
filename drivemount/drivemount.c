fa569a8ac0319e00c5f991677a5d250f384749a2
James Henstridge <jamesh@src.gnome.org>
2004-10-22 08:03:12 +0000

    fix up the copyright headers.

--- drivemount/drivemount.c
+++ drivemount/drivemount.c
@@ -2,11 +2,10 @@
  * Drive Mount Applet
  * Copyright (c) 2004 Canonical Ltd
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License VERSION 2 as
- * published by the Free Software Foundation.  You are not allowed to
- * use any other version of the license; unless you got the explicit
- * permission from the author to do so.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
