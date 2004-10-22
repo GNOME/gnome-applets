96cfd0308ac95059948d87ebd247b8b859e55e1d
James Henstridge <james@jamesh.id.au>
2004-10-22 08:08:14 +0000

    change the copyright header to the standard GPL, rather than a modified
    2004-10-22  James Henstridge  <james@jamesh.id.au>
    
        * src/trash-monitor.[ch]: change the copyright header to the
        standard GPL, rather than a modified one preventing the use of
        future versions.

--- trashapplet/src/trash-monitor.h
+++ trashapplet/src/trash-monitor.h
@@ -4,20 +4,20 @@
  * Copyright (C) 1999, 2000 Eazel, Inc.
  * Copyright (C) 2004 Canonical Ltd.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License VERSION 2 as
- * published by the Free Software Foundation.  You are not allowed to
- * use any other version of the license; unless you got the explicit
- * permission from the author to do so.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
  */
 
 #ifndef __TRASH_MONITOR_H__
