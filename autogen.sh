a742d1484338bb0c75c9770bb031dc780188dbea
Thomas Vander Stichele <thomasvs@src.gnome.org>
2005-01-03 18:20:37 +0000

    autogen again

--- autogen.sh
+++ autogen.sh
@@ -19,8 +19,4 @@ which gnome-autogen.sh || {
     echo "You need to install gnome-common from the GNOME CVS"
     exit 1
 }
-REQUIRED_AUTOMAKE_VERSION=1.8 \
-USE_GNOME2_MACROS=1 \
-USE_COMMON_DOC_BUILD=yes \
-ACLOCAL_AMFLAGS=-I m4 \
-. gnome-autogen.sh
+REQUIRED_AUTOMAKE_VERSION=1.8 USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes ACLOCAL_AMFLAGS=-I m4 . gnome-autogen.sh
