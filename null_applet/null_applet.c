eb0e4004a3632b28bf65f04ccbb205b8c2a93353
Kjartan Maraas <kmaraas@gnome.org>
2005-02-24 09:16:24 +0000

    Don't mix code and decl.
    2005-02-24  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* null_applet.c: (applet_factory):  Don't mix code and decl.
    
    2005-01-07  Kjartan Maraas  <kmaraas@gnome.org>

--- null_applet/null_applet.c
+++ null_applet/null_applet.c
@@ -134,6 +134,7 @@ applet_factory (PanelApplet *applet,
 		gpointer     user_data)
 {
 	char *applet_list;
+	GtkWidget *dialog;
 
 	if (already_running)
 	{
@@ -144,8 +145,6 @@ applet_factory (PanelApplet *applet,
 	g_warning ("was given applet to handle\n");
 
 	applet_list = get_all_applets ();
-
-	GtkWidget *dialog;
 
 	dialog = gtk_message_dialog_new_with_markup (NULL,
 			GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
