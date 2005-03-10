8cf31d49fc1812de41f33c10fb419bd93ef86aad
Maxim Dziumanenko <mvd@mylinux.com.ua>
2005-03-10 18:09:38 +0000

    Add Ukrainian translation of the manual.
    2005-03-10  Maxim Dziumanenko <mvd@mylinux.com.ua>
    
    	* Add Ukrainian translation of the manual.

---
+++ accessx-status/docs/uk/generate_help.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+xml2po -p uk.po ../../../accessx-status/docs/C/accessx-status.xml  > ../../../accessx-status/docs/uk/accessx-status.xml
+xml2po -p uk.po ../../../battstat/docs/C/battstat.xml > ../../../battstat/docs/uk/battstat.xml 
+xml2po -p uk.po ../../../charpick/help/C/char-palette.xml > ../../../charpick/help/uk/char-palette.xml 
+xml2po -p uk.po ../../../cpufreq/help/C/cpufreq-applet.xml > ../../../cpufreq/help/uk/cpufreq-applet.xml 
+xml2po -p uk.po ../../../drivemount/help/C/drivemount.xml > ../../../drivemount/help/uk/drivemount.xml 
+xml2po -p uk.po ../../../geyes/docs/C/geyes.xml > ../../../geyes/docs/uk/geyes.xml 
+xml2po -p uk.po ../../../gswitchit/help/C/gswitchit.xml > ../../../gswitchit/help/uk/gswitchit.xml
+xml2po -p uk.po ../../../gtik/help/C/gtik2_applet2.xml > ../../../gtik/help/uk/gtik2_applet2.xml 
+xml2po -p uk.po ../../../gweather/docs/C/gweather.xml > ../../../gweather/docs/uk/gweather.xml 
+xml2po -p uk.po ../../../mini-commander/help/C/command-line.xml > ../../../mini-commander/help/uk/command-line.xml 
+xml2po -p uk.po ../../../mixer/docs/C/mixer_applet2.xml > ../../../mixer/docs/uk/mixer_applet2.xml 
+xml2po -p uk.po ../../../multiload/docs/C/multiload.xml > ../../../multiload/docs/uk/multiload.xml 
+xml2po -p uk.po ../../../stickynotes/docs/C/stickynotes_applet.xml > ../../../stickynotes/docs/uk/stickynotes_applet.xml 
+xml2po -p uk.po ../../../trashapplet/docs/C/trashapplet.xml > ../../../trashapplet/docs/uk/trashapplet.xml
