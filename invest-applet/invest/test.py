0682836e2d0478e7d1cc78e2d3dc2fd2885aa03a
Callum McKenzie <callum@src.gnome.org>
2008-11-08 01:50:58 +0000

    Insert a pre-prepared stock list so we no longer need to have run the invest applet before running the tests.
    svn path=/trunk/; revision=11110

--- invest-applet/invest/test.py
+++ invest-applet/invest/test.py
@@ -5,25 +5,14 @@ import unittest
 import quotes
 import invest
 
-print """=====================================================================
-Please note that these tests will fail if you do not have any stocks
-already set up in the applet. This is an unfortunate state of affairs
-that we hope to have fixed in the near future. For now, please run
-the applet and enter at least one stock to be monitored. The default
-is just fine.
-====================================================================="""
-
 def null_function (*args):
     pass
 
 class TestQuotes (unittest.TestCase):
     def testQuoteUpdater_populate (self):
         qu = quotes.QuoteUpdater (null_function, null_function)
-        # We require a pre-created invest.STOCKS for the moment (since
-        # the quotes module will load the real database). Pick the first
-        # stock as the one we test.
-        goodname = invest.STOCKS.keys()[0]
-        quote = { goodname: { "ticker": goodname, "trade": 386.91, "time": "10/3/2008", "date": "4.00pm", "variation": -3.58, "open": 397.14, "variation_pct": 10 }}
+        invest.STOCKS = {'GOGO': [{'amount' : 1, 'comission' : 0.0, 'bought': 0.0}], 'JAVA': [{'amount' : 1, 'comission' : 0.0, 'bought': 0.0}]}
+        quote = { 'GOGO': { "ticker": 'GOGO', "trade": 386.91, "time": "10/3/2008", "date": "4.00pm", "variation": -3.58, "open": 397.14, "variation_pct": 10 }}
         qu.populate (quote)
         self.assertEqual (qu.quotes_valid, True)
         # In response to bug 554425, try a stock that isn't in our database
