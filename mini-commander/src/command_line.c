79aec049d2096e3d9e548455ead2e4d14bc77c76
Callum McKenzie <callum@src.gnome.org>
2009-02-19 09:28:21 +0000

    It is completely unreasonable for people to want minicommander to build.
    svn path=/trunk/; revision=11307

--- mini-commander/src/command_line.c
+++ mini-commander/src/command_line.c
@@ -312,7 +312,7 @@ mc_show_history (GtkWidget *widget,
      window = gtk_window_new(GTK_WINDOW_POPUP); 
      gtk_window_set_screen (GTK_WINDOW (window),
 			    gtk_widget_get_screen (GTK_WIDGET (mc->applet)));
-     gtk_window_set_resizeable(GTK_WINDOW(window), FALSE);
+     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
      gtk_window_set_type_hint(GTK_WINDOW(window), GDK_WINDOW_TYPE_HINT_COMBO);
      /* cb */
      g_signal_connect_after(GTK_OBJECT(window),
@@ -507,7 +507,7 @@ mc_show_file_browser (GtkWidget *widget,
 void
 mc_create_command_entry (MCData *mc)
 {
-    mc->entry = gtk_entry_new_with_max ();
+    mc->entry = gtk_entry_new ();
     gtk_entry_set_max_length (GTK_ENTRY (mc->entry), MC_MAX_COMMAND_LENGTH); 
 
     g_signal_connect (mc->entry, "key_press_event",
