0c6236a17901383498198f4bc181de35a744cc00
Sebastian Geiger <sbastig@gmx.net>
2012-02-06 00:30:30 +0100

    task-title: multiple changes
     * Change from h/v box to GtkGrid
     * Remove gconf and replace with GSettings
     * Use the gsettings pointer from common.h
       to access the settings object created
       during applet initialization
     * Clean up the init function and add two
       helper functions to create the button
       and label of the title
     * Clean up the icon loading for the
       logoff button

--- src/task-title.c
+++ src/task-title.c
@@ -31,6 +31,7 @@
 #include <gconf/gconf.h>
 #include <gconf/gconf-client.h>
 
+#include "common.h"
 #include "task-list.h"
 
 G_DEFINE_TYPE (TaskTitle, task_title, GTK_TYPE_EVENT_BOX);
@@ -39,14 +40,14 @@ G_DEFINE_TYPE (TaskTitle, task_title, GTK_TYPE_EVENT_BOX);
   TASK_TYPE_TITLE, \
   TaskTitlePrivate))
 
-#define SHOW_HOME_TITLE_KEY "/apps/window-picker-applet/show_home_title"
+#define SHOW_HOME_TITLE_KEY "show-home-title"
 
 struct _TaskTitlePrivate
 {
     WnckScreen *screen;
     WnckWindow *window;
     GtkWidget *align;
-    GtkWidget *box;
+    GtkWidget *grid;
     GtkWidget *label;
     GtkWidget *button;
     GtkWidget *button_image;
@@ -170,11 +171,11 @@ static void on_state_changed (WnckWindow *window,
         return;
     if (wnck_window_is_maximized (window)) {
         gtk_widget_set_state (GTK_WIDGET (title), GTK_STATE_ACTIVE);
-        gtk_widget_show (priv->box);
+        gtk_widget_show (priv->grid);
     } else {
         gtk_widget_set_state (GTK_WIDGET (title), GTK_STATE_NORMAL);
-        gtk_widget_hide (priv->box);
-    }  
+        gtk_widget_hide (priv->grid);
+    }
 }
 
 /**
@@ -229,13 +230,13 @@ static void on_active_window_changed (WnckScreen *screen,
                 _("Log off, switch user, lock screen or power "
                 "down the computer"));
             gtk_widget_set_tooltip_text (GTK_WIDGET (title),
-                _("Home"));      
-            gtk_widget_show (priv->box);
-        } else {
+                _("Home"));
+            gtk_widget_show (priv->grid);
+        } else { //reset the task title and hide it
             gtk_widget_set_state (GTK_WIDGET (title), GTK_STATE_NORMAL);
             gtk_widget_set_tooltip_text (priv->button, NULL);
             gtk_widget_set_tooltip_text (GTK_WIDGET (title), NULL);
-            gtk_widget_hide (priv->box);
+            gtk_widget_hide (priv->grid);
         }
     } else { //the new active window is a regular window
         gtk_label_set_text (GTK_LABEL (priv->label),
@@ -251,7 +252,7 @@ static void on_active_window_changed (WnckScreen *screen,
             G_CALLBACK (on_icon_changed), title);
         g_signal_connect_after (act_window, "state-changed",
             G_CALLBACK (on_state_changed), title);
-        gtk_widget_show (priv->box);  
+        gtk_widget_show (priv->grid);
         priv->window = act_window;
     }
 
@@ -260,7 +261,7 @@ static void on_active_window_changed (WnckScreen *screen,
         && (priv->show_home_title ? type != WNCK_WINDOW_DESKTOP : 1))
     {
         gtk_widget_set_state (GTK_WIDGET (title), GTK_STATE_NORMAL);
-        gtk_widget_hide (priv->box);
+        gtk_widget_hide (priv->grid);
     } else if (!WNCK_IS_WINDOW (act_window)) {
         if (task_list_get_desktop_visible (TASK_LIST (task_list_get_default ()))
             && priv->show_home_title)
@@ -272,13 +273,13 @@ static void on_active_window_changed (WnckScreen *screen,
                 _("Log off, switch user, lock screen or power "
                 "down the computer"));
             gtk_widget_set_tooltip_text (GTK_WIDGET (title),
-                _("Home"));      
-            gtk_widget_show (priv->box);
-        } else {  
+                _("Home"));
+            gtk_widget_show (priv->grid);
+        } else {
             gtk_widget_set_state (GTK_WIDGET (title), GTK_STATE_NORMAL);
             gtk_widget_set_tooltip_text (priv->button, NULL);
             gtk_widget_set_tooltip_text (GTK_WIDGET (title), NULL);
-            gtk_widget_hide (priv->box);
+            gtk_widget_hide (priv->grid);
         }
     } else gtk_widget_set_state (GTK_WIDGET (title), GTK_STATE_ACTIVE);
     gtk_widget_queue_draw (GTK_WIDGET (title));
@@ -354,87 +355,94 @@ static void task_title_class_init (TaskTitleClass *klass) {
     g_type_class_add_private (obj_class, sizeof (TaskTitlePrivate));
 }
 
-static void task_title_init (TaskTitle *title) {
-    TaskTitlePrivate *priv;
-    GConfClient *client;
-    GdkScreen *gdkscreen;
-    GtkIconTheme *theme;
-    GtkSettings *settings;
-    GdkPixbuf *pixbuf;
-    AtkObject *atk;
-    int width, height;
-    
-    priv = title->priv = TASK_TITLE_GET_PRIVATE (title);
-    priv->screen = wnck_screen_get_default ();
-    priv->window = NULL;
-    client = gconf_client_get_default ();
-    priv->show_home_title = gconf_client_get_bool (
-        client,
-        SHOW_HOME_TITLE_KEY,
-        NULL
-    );
-    g_object_unref (client);
-    gtk_widget_add_events (GTK_WIDGET (title), GDK_ALL_EVENTS_MASK);
-    priv->align = gtk_alignment_new (0.0, 0.5, 1.0, 1.0);
-    gtk_alignment_set_padding (GTK_ALIGNMENT (priv->align), 
-        0, 0, 6, 6);
-    gtk_container_add (GTK_CONTAINER (title), priv->align);
-    priv->box = gtk_hbox_new (FALSE, 2);
-    gtk_container_add (GTK_CONTAINER (priv->align), priv->box);
-    gtk_widget_set_no_show_all (priv->box, TRUE);
-    gtk_widget_show (priv->box);
-    priv->label = gtk_label_new (_("Home"));
-    gtk_label_set_ellipsize (GTK_LABEL (priv->label), PANGO_ELLIPSIZE_END);
-    gtk_misc_set_alignment (GTK_MISC (priv->label), 0.0, 0.5);
+static GtkWidget *getTitleLabel() {
+    GtkWidget *label = gtk_label_new (_("Home"));
+    gtk_label_set_ellipsize (GTK_LABEL (label), PANGO_ELLIPSIZE_END);
+    gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
     PangoAttrList *attr_list = pango_attr_list_new ();
     PangoAttribute *attr = pango_attr_weight_new (PANGO_WEIGHT_BOLD);
     pango_attr_list_insert (attr_list, attr);
-    gtk_label_set_attributes (GTK_LABEL (priv->label), attr_list);  
-    gtk_box_pack_start (GTK_BOX (priv->box), priv->label, TRUE, TRUE, 0);
-    gtk_widget_show (priv->label);
+    gtk_label_set_attributes (GTK_LABEL (label), attr_list);
+    gtk_widget_set_hexpand (label, TRUE);
+    gtk_widget_set_vexpand (label, TRUE);
+    gtk_widget_set_valign (label, GTK_ALIGN_CENTER);
+    return label;
+}
 
-    priv->button = g_object_new (
-        GTK_TYPE_EVENT_BOX, 
+static GtkWidget *getCloseButton(TaskTitle* title) {
+    //Create the button
+    GtkWidget *button = g_object_new (
+        GTK_TYPE_EVENT_BOX,
         "visible-window", FALSE,
         "above-child", TRUE,
         NULL
     );
-    gtk_box_pack_start (GTK_BOX (priv->box), priv->button, FALSE, FALSE, 0);
-    gtk_widget_show (priv->button);
-    atk = gtk_widget_get_accessible (priv->button);
+
+    //Add accessibility
+    AtkObject *atk = gtk_widget_get_accessible (button);
     atk_object_set_name (atk, _("Close"));
     atk_object_set_description (atk, _("Close current window."));
-    atk_object_set_role (atk, ATK_ROLE_PUSH_BUTTON);  
-    g_signal_connect (priv->button, "button-release-event", 
+    atk_object_set_role (atk, ATK_ROLE_PUSH_BUTTON);
+
+    //Connect the signals
+    g_signal_connect (button, "button-release-event",
         G_CALLBACK (on_close_clicked), title);
-    g_signal_connect (priv->button, "enter-notify-event",
+    g_signal_connect (button, "enter-notify-event",
         G_CALLBACK (on_enter_notify), title);
-    g_signal_connect (priv->button, "leave-notify-event",
+    g_signal_connect (button, "leave-notify-event",
         G_CALLBACK (on_leave_notify), title);
-    g_signal_connect (priv->button, "draw",
+    g_signal_connect (button, "draw",
         G_CALLBACK (on_button_draw), title);
+
+    return button;
+}
+
+static void task_title_init (TaskTitle *title) {
+    GSettings *gsettings = mainapp->settings;
+    int width, height;
+    TaskTitlePrivate *priv = title->priv = TASK_TITLE_GET_PRIVATE (title);
+    priv->screen = wnck_screen_get_default ();
+    priv->window = NULL;
+    priv->show_home_title = g_settings_get_boolean (
+        gsettings, SHOW_HOME_TITLE_KEY
+    );
+    gtk_widget_add_events (GTK_WIDGET (title), GDK_ALL_EVENTS_MASK);
+    priv->align = gtk_alignment_new (0.0, 0.0, 1.0, 1.0);
+    gtk_alignment_set_padding (GTK_ALIGNMENT (priv->align),
+        0, 0, 6, 6);
+    gtk_container_add (GTK_CONTAINER (title), priv->align);
+    priv->grid = gtk_grid_new();
+    gtk_grid_set_row_spacing (GTK_GRID(priv->grid), 2);
+    gtk_container_add (GTK_CONTAINER (priv->align), priv->grid);
+    gtk_widget_set_no_show_all (priv->grid, TRUE);
+    gtk_widget_show (priv->grid);
+
+    //Prepare and show the title label
+    priv->label = getTitleLabel();
+    gtk_grid_attach (GTK_GRID(priv->grid), priv->label, 0, 0, 1, 1);
+    gtk_widget_show (priv->label);
+
+    //Create the close button
+    priv->button = getCloseButton (title);
+    gtk_grid_attach (GTK_GRID(priv->grid), priv->button, 1, 0, 1, 1);
+    gtk_widget_show (priv->button);
+
     g_signal_connect (GTK_WIDGET(title), "draw",
         G_CALLBACK (on_draw), title);
-    /* Load the quit icon.  We have to do this in such a god-forsaken way
-        because of http://bugzilla.gnome.org/show_bug.cgi?id=581359 and the
-        fact that we support as far back as GTK+ 2.12 (which never passes
-        FORCE_SIZE in GtkImage).  The only way to guarantee icon size is to
-        load and scale it ourselves.  We don't do this for all the other icons
-        in the source just because we know that this icon doesn't come in the
-        size we want. */
-    gdkscreen = gtk_widget_get_screen (GTK_WIDGET (title));
-    theme = gtk_icon_theme_get_for_screen (gdkscreen);
-    settings = gtk_settings_get_for_screen (gdkscreen);
-    if (!gtk_icon_size_lookup_for_settings (settings, GTK_ICON_SIZE_MENU, 
-            &width, &height)) 
-    {
-        width = height = 16;
-    }
-    width = MIN(width, height);
-    pixbuf = gtk_icon_theme_load_icon (theme, "gnome-logout", width, 0, NULL);
-    priv->quit_icon = gdk_pixbuf_scale_simple (pixbuf, width, width,
-        GDK_INTERP_BILINEAR);
-    g_object_unref (G_OBJECT (pixbuf));
+
+    // Prepare and add the logoff icon to the title
+    GdkScreen *gdkscreen = gtk_widget_get_screen (GTK_WIDGET (title));
+    GtkIconTheme *theme = gtk_icon_theme_get_for_screen (gdkscreen);
+    GtkSettings *settings = gtk_settings_get_for_screen (gdkscreen);
+    gtk_icon_size_lookup_for_settings (
+        settings,
+        GTK_ICON_SIZE_MENU,
+        &width, &height
+    );
+    //this shows a little green exit icon, like the ones on emergency exits
+    priv->quit_icon = gtk_icon_theme_load_icon (
+        theme, "gnome-logout", width, 0, NULL
+    );
     priv->button_image = gtk_image_new_from_pixbuf (priv->quit_icon);
     gtk_container_add (GTK_CONTAINER (priv->button), priv->button_image);
     gtk_widget_show (priv->button_image);
@@ -446,7 +454,7 @@ static void task_title_init (TaskTitle *title) {
     if (priv->show_home_title)
         gtk_widget_set_state (GTK_WIDGET (title), GTK_STATE_ACTIVE);
     else
-        gtk_widget_hide (priv->box);
+        gtk_widget_hide (priv->grid);
     gtk_widget_add_events (GTK_WIDGET (title), GDK_ALL_EVENTS_MASK);
 
     g_signal_connect (priv->screen, "active-window-changed",
