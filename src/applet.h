5c7dcb95cd17a52e9651d2fe0a81de14bc2e61f8
Sebastian Geiger <sbastig@gmx.net>
2014-10-07 12:01:03 +0200

    Add my self as author

--- src/applet.h
+++ src/applet.h
@@ -13,6 +13,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by Sebastian Geiger <sbastig@gmx.net>
  */
 
 #ifndef _WINDOW_PICKER_APPLET_H_
--- src/task-item.c
+++ src/task-item.c
@@ -14,7 +14,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Authored by Jason Smith <jassmith@gmail.com>
+ * Authored by: Jason Smith <jassmith@gmail.com>
+ *              Sebastian Geiger <sbastig@gmx.net>
  */
 
 #include "task-item.h"
