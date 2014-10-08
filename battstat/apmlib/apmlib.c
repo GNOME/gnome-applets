76edd5f99effc679726bf72e3151e19ccfb4aacf
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2014-10-09 01:24:58 +0300

    upgrade LGPL license to GPL
    GNOME Applets are licensed under GPL (v2 or later) so change LGPL
    license to GPL. (LGPL license allows this move)

--- battstat/apmlib/apmlib.c
+++ battstat/apmlib/apmlib.c
@@ -3,19 +3,18 @@
  * Revised: Fri Dec 26 21:38:29 1997 by faith@acm.org
  * Copyright 1996, 1997 Rickard E. Faith (faith@acm.org)
  * 
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Library General Public License as published
- * by the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- * 
- * This library is distributed in the hope that it will be useful, but
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- * 
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
  */
 #include <stdio.h>
