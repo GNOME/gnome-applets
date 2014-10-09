94efc723206dfa1c5504fae3b292d0a68127faf0
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2014-10-09 03:39:12 +0300

    gweather: update license headers
    This applet is licensed under GPL without specified version number,
    update license to GPLv2 or any later version.

--- gweather/gweather-about.h
+++ gweather/gweather-about.h
@@ -1,17 +1,22 @@
+/*
+ * Copyright (C) Papadimitriou Spiros <spapadim+@cs.cmu.edu>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __GWEATHER_ABOUT_H_
 #define __GWEATHER_ABOUT_H_
-
-/* $Id$ */
-
-/*
- *  Papadimitriou Spiros <spapadim+@cs.cmu.edu>
- *
- *  This code released under the GNU GPL.
- *  Read the file COPYING for more information.
- *
- *  About box
- *
- */
 
 #define GWEATHER_I_KNOW_THIS_IS_UNSTABLE
 
--- gweather/gweather.h
+++ gweather/gweather.h
@@ -1,15 +1,23 @@
+/*
+ * Copyright (C) todd kulesza <fflewddur@dropline.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __GWEATHER_H__
 #define __GWEATHER_H__
 
-/*
- *  todd kulesza <fflewddur@dropline.net>
- *
- *  This code released under the GNU GPL.
- *  Read the file COPYING for more information.
- *
- * main header file
- *
- */
 #include <glib/gi18n.h>
 
 #include <libgweather/gweather-weather.h>
