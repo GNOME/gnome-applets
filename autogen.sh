51cb2b92d95183e2ffbe28caa33c0921f3c624a5
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2015-03-24 18:04:47 +0200

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,22 +4,19 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="Gnome Applets"
-
 (test -f $srcdir/configure.ac \
-  && test -f $srcdir/HACKING \
-  && test -d $srcdir/gweather) || {
+  && test -f $srcdir/gnome-applets.doap) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
-    echo " top-level gnome directory"
+    echo " top-level gnome-applets directory"
     exit 1
 }
-
 
 which gnome-autogen.sh || {
-    echo "You need to install gnome-common from the GNOME CVS"
+    echo "You need to install gnome-common."
     exit 1
 }
-REQUIRED_AUTOMAKE_VERSION=1.9 \
+
+REQUIRED_AUTOMAKE_VERSION=1.9
 REQUIRED_YELP_TOOLS_VERSION=3.1.1
-USE_GNOME2_MACROS=1 \
+
 . gnome-autogen.sh
