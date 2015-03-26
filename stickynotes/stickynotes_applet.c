2962ee53a3d2f8527096b16f9628986b743b6c14
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2015-03-26 11:44:41 +0200

    stickynotes: ui, split out preferences dialog...
    ... and update to newer GTK+ version.

--- stickynotes/stickynotes_applet.c
+++ stickynotes/stickynotes_applet.c
@@ -191,7 +191,7 @@ void stickynotes_applet_init_prefs(void)
 
 	stickynotes->builder = gtk_builder_new ();
 
-        gtk_builder_add_from_file (stickynotes->builder, BUILDER_PATH, NULL);
+        gtk_builder_add_from_file (stickynotes->builder, GTK_BUILDERDIR "/stickynotes-preferences.ui", NULL);
 
 	stickynotes->w_prefs = GTK_WIDGET (gtk_builder_get_object (stickynotes->builder,
 			"preferences_dialog"));
