85643680eb0a76b10050c916c21f71e8571294fd
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2017-03-28 13:45:38 +0300

    window-title: update license headers

--- window-title/windowtitle.c
+++ window-title/windowtitle.c
@@ -1,19 +1,21 @@
 /*
- * main.c
- * Copyright (C) Andrej Belcijan 2009 <andrej.{@}.gmail.dot.com>
+ * Copyright (C) 2009 Andrej Belcijan
  *
- * main.c is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 2 of the License, or
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * main.c is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Andrej Belcijan <{andrejx} at {gmail.com}>
  */
 
 #include "windowtitle.h"
