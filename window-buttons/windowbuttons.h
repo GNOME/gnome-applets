aacb0f0cf7f7ee1e5d167670adadc0a6b032efb3
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2017-03-28 12:01:58 +0300

    window-buttons: update license headers

--- window-buttons/windowbuttons.h
+++ window-buttons/windowbuttons.h
@@ -1,25 +1,21 @@
-/***************************************************************************
- *            windowbuttons.h
- *
- *  Mon May  4 01:21:56 2009
- *  Copyright  2009  Andrej Belcijan
- *  <{andrejx} at {gmail.com}>
- ****************************************************************************/
-
 /*
- * This program is free software; you can redistribute it and/or modify
+ * Copyright (C) 2009 Andrej Belcijan
+ *
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Library General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *     Andrej Belcijan <{andrejx} at {gmail.com}>
  */
 
 #define PLAINTEXT_CONFIG				0
