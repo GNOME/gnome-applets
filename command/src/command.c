05d937d98bed48da7a71c76177edafdaba168a5f
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2020-03-26 05:19:43 +0200

    command: really fix invalid icon size warning

--- command/src/command.c
+++ command/src/command.c
@@ -338,7 +338,7 @@ command_applet_fill (PanelApplet* applet)
     command_applet->width = g_settings_get_int (command_applet->settings, WIDTH_KEY);
 
     command_applet->box = GTK_BOX (gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0));
-    command_applet->image = GTK_IMAGE (gtk_image_new_from_icon_name (APPLET_ICON, 24));
+    command_applet->image = GTK_IMAGE (gtk_image_new_from_icon_name (APPLET_ICON, GTK_ICON_SIZE_LARGE_TOOLBAR));
     command_applet->label = GTK_LABEL (gtk_label_new (ERROR_OUTPUT));
     command_applet->timeout_id = 0;
 
